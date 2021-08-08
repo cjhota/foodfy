60ae54ba83fb4fb9b7ddad5aaf5c666ed0daa7a8
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-08-08 07:04:41 -0300

    estruturação do modulo admin

--- src/app/controllers/recepes.js
+++ src/app/controllers/recepes.js
@@ -1,5 +1,14 @@
 module.exports = {
     index(req, res) {
         return res.render("admin/receitas/index")
+    },
+    create(req, res) {
+        return res.render("admin/receitas/create")
+    },
+    edit(req, res) {
+        return res.render("admin/receitas/edit")
+    },
+    details(req, res) {
+        return res.render("admin/receitas/details")
     }
 }
