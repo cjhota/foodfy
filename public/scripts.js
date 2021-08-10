2fd9dce19d49571c1dfa1fcd4171d706987d7572
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-08-09 21:18:53 -0300

    Ajustes no scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,54 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll("header .links a")
+// const currentPage = location.pathname
+// const menuItems = document.querySelectorAll("header .links a")
 
-for (item of menuItems) {
+// for (item of menuItems) {
 
-  if (currentPage.includes(item.getAttribute("href"))) {
-    item.classList.add("active")
-  }
+//   if (currentPage.includes(item.getAttribute("href"))) {
+//     item.classList.add("active")
+//   }
+// }
+
+// adicionar items
+function addIngredient() {
+  const ingredients = document.querySelector("#ingredients");
+  const fieldContainer = document.querySelectorAll(".ingredient");
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
 }
+
+document
+  .querySelector(".add-ingredient")
+  .addEventListener("click", addIngredient);
+
+  
+// adicionar items
+function addProximoEvent() {
+  const ingredients = document.querySelector("#prox_passos");
+  const fieldContainer = document.querySelectorAll(".prox_passo");
+
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+document
+  .querySelector(".add-prox_passo")
+  .addEventListener("click", addProximoEvent);
+
 
 // Redirect for recipe details
 const cards = document.querySelectorAll('.card');
@@ -39,23 +81,3 @@ function hideDetails(index) {
 for (let i = 0; i < 3; i++) {
   hideDetails(i)
 }
-
-// adicionar items
-function addIngredient() {
-  const ingredients = document.querySelector("#ingredients");
-  const fieldContainer = document.querySelectorAll(".ingredient");
-
-  // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-
-  // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
-
-  // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
-}
-
-document
-  .querySelector(".add-ingredient")
-  .addEventListener("click", addIngredient);
