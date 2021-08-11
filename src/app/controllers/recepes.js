bc3dc0136c819cbb88c1a51ef6e8f2cc465e72a2
Jose Ribeiro <joseribeirocsr@gmail.com>
2021-08-11 19:33:31 -0300

    CONSTRUÇÃO DO SHOW

--- src/app/controllers/recepes.js
+++ src/app/controllers/recepes.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require("../../../data.json")
+
 module.exports = {
     index(req, res) {
         return res.render("admin/receitas/index")
@@ -5,10 +8,24 @@ module.exports = {
     create(req, res) {
         return res.render("admin/receitas/create")
     },
+    show(req, res) {
+        const {
+            id
+        } = req.params
+    
+        const foundRecepet = data.recepes.find(function (recepet) {
+            return recepet.id == id
+        })
+    
+        if (!foundRecepet) return res.send("recepet not found !")
+
+
+
+        return res.render("admin/receitas/show"), {
+            recepet
+        }
+    },
     edit(req, res) {
         return res.render("admin/receitas/edit")
     },
-    show(req, res) {
-        return res.render("admin/receitas/show")
-    }
 }
--- src/routes.js
+++ src/routes.js
@@ -32,8 +32,8 @@ routes.get("/receitas/:index", (req, res) => {
 
 routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
 routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
-routes.get("/admin/recipes/show", recipes.show); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+// routes.get("/admin/recipes/show", recipes.show); // Mostrar formulário de nova receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita;
 routes.get("/admin/recipes/edit", recipes.edit); // Mostrar formulário de edição de receita;
 
