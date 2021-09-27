d7daf475889db582af929f59d0a58f97fb5d2ead
José Ribeiro <joseribeirocsr@gmail.com>
2021-09-27 19:57:38 -0300

    .

--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -1,41 +1,29 @@
 // const fs = require("fs");
-// // const data = require("../../../data.json");
+const data = require("../../../data.json");
 const recipes = require("../../data");
 // const receitas = require("../../data");
 
 module.exports= {
 
 index (req, res) {
-    // console.log(recipes)
     return res.render("users/index", { items:recipes  })//{ items: data.recipes }
 },                                      
 
 about (req, res) {
-    // console.log(recipes)
-
     return res.render("users/about")
 },
 
 recipes (req, res) {
-    // console.log(recipes)
-
-    // return res.send("Estou aqui")
     return res.render("users/recipes", { items: recipes  })
 },
 
 show (req, res) {
   const recipeIndex = req.params.index
-//   recipes[recipeIndex]
   const recipe = [...recipes]
 
-  res.render('users/recipe', {  recipes: recipe[recipeIndex] }) //recipe: recipe[recipeIndex] ,
+  const recipe = recipes.recipes[recipeIndex]
+
+  res.render('users/recipe', {  recipes: recipe[recipeIndex] })
 }
-//     const recipeIndex = req.params.index;
-//     // data[recipeIndex]
-//     const recipe = [...recipe];
 
-//     // const recipe = data.recipes[recipeIndex]
-//     console.log(recipe)
-
-//     return res.render("users/recipe", { recipe: recipe[recipeIndex]})//items: recipe{ items: recipe }
 }
